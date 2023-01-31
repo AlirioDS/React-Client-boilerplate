d5bdcd8ff50b198205752825f60a99b980f8ad0d
AlirioDS <silverneonstaring@gmail.com>
2023-01-31 19:39:28 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Client React
+## Instalacion
+1. instalar docker y docker-compose
+2. Levantar Docker compose `docker-compose-dev.yml`
+   ```bash
+    docker-compose -f docker-compose.yml -f docker-compose-dev.yml up -d --build
+   ```
+5. Ir `http://localhost:3000/`
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p style={{ fontSize: "3rem" }}>
+          Cliente
+        </p>
+      </header>
+      <footer className="App-footer">
+        <p style={{ fontSize: "1.8rem"}}>
+          Copyright &copy; Todos los Derechos Reservados.
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
